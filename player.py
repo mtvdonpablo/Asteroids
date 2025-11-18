f3fc93f5524b8aa29e07cb86b0360e9850da3d0e
mtvdonpablo <djoseph@opentext.com>
2025-11-18 16:52:21 -0500

    added w and s key to move up and down

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS,LINE_WIDTH,PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS,LINE_WIDTH,PLAYER_TURN_SPEED,PLAYER_SPEED
 import pygame
 class Player(CircleShape):
     def __init__(self,x,y):
@@ -17,6 +17,11 @@ class Player(CircleShape):
         pygame.draw.polygon(screen,"white", self.triangle(),LINE_WIDTH)
     def rotate(self,dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+    def move(self,dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -24,3 +29,7 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
