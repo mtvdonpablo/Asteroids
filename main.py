791fea26b718a8c261ff7623edec8641b24e8809
mtvdonpablo <djoseph@opentext.com>
2025-11-18 16:47:37 -0500

    added object and rotating left and right abilities

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT,SCREEN_WIDTH 
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver} " )
@@ -9,13 +10,22 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT / 2))
+    
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-        return
+                return
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        time_passed_ms = clock.tick(60)
+        dt = time_passed_ms / 1000
+
         
 
 if __name__ == "__main__":
